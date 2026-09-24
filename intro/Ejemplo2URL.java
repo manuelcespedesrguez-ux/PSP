1efd09390bdf03f25612319069ae5a0aa6b0134b
manuelcespedesrguez-ux <manuelcespedesrguez@gmail.com>
2026-09-24 11:35:29 +0200

    Initial commit

---
+++ intro/Ejemplo2URL.java
@@ -0,0 +1,29 @@
+package intro;
+
+import java.io.*;
+import java.net.*;
+
+public class Ejemplo2URL {
+    public static void main(String[] args) {
+        URL url = null;
+        try {
+            url = new URL("https://www.cebem.es/es");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader in;
+        try {
+            InputStream inputStream = url.openStream();
+            in = new BufferedReader(new InputStreamReader(inputStream));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
