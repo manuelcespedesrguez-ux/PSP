3d9a445f93627b41860b602eaaba57ace64a39cb
manuelcespedesrguez-ux <manuelcespedesrguez@gmail.com>
2026-09-24 14:23:24 +0200

    24/09/2026

---
+++ intro/Ejemplo2URLCon.java
@@ -0,0 +1,41 @@
+package intro;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Ejemplo2URLCon {
+
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://localhost/2014/vernombre.php");
+            URLConnection conexion = url.openConnection();
+            conexion.setDoOutput(true);
+
+            Scanner sc = new  Scanner(System.in);
+            System.out.println("Introduce tu nombre: ");
+            String nombre = sc.nextLine();
+            System.out.println("Introduce los apellidos: ");
+            String apellidos = sc.nextLine();
+            String cadena = "nombre " + nombre + " apellidos=" + apellidos;
+
+            // ESCRIBIR EN LA URL
+            PrintWriter output = new PrintWriter(conexion.getOutputStream());
+            output.write(cadena);
+            output.close(); // cerrar flujo
+
+            // LEER DE LA URL
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                System.out.println(linea);
+            }
+            reader.close();// cerrar flujo
+
+        } catch (MalformedURLException me) {
+            System.err.println("MalformedURLException: " + me);
+        } catch (IOException ioe) {
+            System.err.println("IOException: " + ioe);
+        }
+    }
+}
